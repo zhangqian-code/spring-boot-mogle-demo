fa6d95b645dfd4e94286dc993759a944ef4519f7
tom_glb <996555975@qq.com>
2020-03-29 12:42:15 +0800

    Update RateLimiter.java

--- spring-boot-mogle-demo-ratelimit-redis/src/main/java/com/mogle/ratelimit/redis/annotation/RateLimiter.java
+++ spring-boot-mogle-demo-ratelimit-redis/src/main/java/com/mogle/ratelimit/redis/annotation/RateLimiter.java
@@ -13,7 +13,32 @@ import java.util.concurrent.TimeUnit;
  *
  */
 @Target(ElementType.METHOD)
-
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RateLimiter {
 
+	long DEFAULT_REQUEST = 10;
+	/**
+	 * max 最大请求数
+	 */
+	@AliasFor("max") long value() default DEFAULT_REQUEST;
+	
+	/**
+	 * max 最大请求数
+	 */
+	@AliasFor("value") long max() default DEFAULT_REQUEST;
+	
+	/**
+	 * 限流key
+	 */
+	String key() default "";
+	
+	/**
+	 * 超时时长，默认1分钟
+	 */
+	long timeout() default 1;
+	
+	/**
+	 * 超时时间单位，默认 分钟
+	 */
+	TimeUnit timeUnit() default TimeUnit.MINUTES;
 }
